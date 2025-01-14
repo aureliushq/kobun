024d2f7c16f2f11720803bc1d2e0405a1871ec95
Ilango <rajagopalilango@gmail.com>
2025-01-14 20:28:51 +0530

    Use vite react-ts template for core

---
+++ packages/core/vite.config.ts
@@ -0,0 +1,43 @@
+import { resolve } from 'node:path'
+import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import * as packageJson from './package.json'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		react(),
+		dts({
+			insertTypesEntry: true,
+		}),
+	],
+	build: {
+		lib: {
+			// Entry point for your library
+			entry: resolve(__dirname, 'src/index.tsx'),
+			// Library name that will be used in UMD builds
+			name: 'rescribe-core',
+			// Generates .cjs.js, .es.js, and .umd.js files
+			fileName: (format) => `rescribe-core.${format}.js`,
+			// Supported formats
+			formats: ['es', 'umd', 'cjs'],
+		},
+		rollupOptions: {
+			// Externalize peer dependencies
+			external: [...Object.keys(packageJson.peerDependencies || {})],
+			output: {
+				// Global variables to use in UMD build for externalized deps
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM',
+				},
+			},
+		},
+		// Generate sourcemaps
+		sourcemap: true,
+		// Ensure clean build directory
+		emptyOutDir: true,
+	},
+})
