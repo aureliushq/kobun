e88632017d7d61cf580871934744793f69c75e07
Ilango <rajagopalilango@gmail.com>
2025-01-16 08:17:42 +0530

    Add context provider for configuration

---
+++ packages/core/src/providers.tsx
@@ -0,0 +1,20 @@
+import { createContext, type ReactNode } from 'react'
+import type { Config } from './types'
+
+type RescribeContextData = {
+	config: Config
+}
+
+const RescribeContext = createContext<RescribeContextData | null>(null)
+
+type RescribeProviderProps = RescribeContextData & {
+	children: ReactNode
+}
+
+const RescribeProvider = ({ config, children }: RescribeProviderProps) => (
+	<RescribeContext.Provider value={{ config }}>
+		{children}
+	</RescribeContext.Provider>
+)
+
+export { RescribeProvider }
