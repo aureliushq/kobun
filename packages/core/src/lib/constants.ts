e3aeac15bf9290630d7a6be36b12bc5b9055ecbf
Ilango <rajagopalilango@gmail.com>
2025-01-17 11:43:03 +0530

    Add storage option to rescribe configuration - decides where to store content and assets

--- packages/core/src/lib/constants.ts
+++ packages/core/src/lib/constants.ts
@@ -3,7 +3,9 @@ import { z } from 'zod'
 export const BASE_PATH = '/rescribe'
 export const BASE_PATH_REGEX = /^\/rescribe\/?/
 
-export const REMIX_BASE_PATH = '/app'
+// TODO: move to server package
+export const REMIX_APP_BASE_PATH = '/app'
+export const REMIX_APP_PUBLIC_PATH = '/public'
 
 export const COLLECTION_SLUG_REGEX = z
 	.string()
--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,10 +1,13 @@
 import { z } from 'zod'
 import type { COLLECTION_SLUG_REGEX } from './lib/constants'
 
-type CollectionFormat = 'md' | 'mdx'
+const CONTENT_FORMAT = z.enum(['md', 'mdx'])
+type ContentFormat = z.infer<typeof CONTENT_FORMAT>
 
 type Glob = '*' | '**'
-type ContentPath = `${string}/${Glob}` | `${string}/${Glob}/${string}`
+type Path = `${string}/${Glob}` | `${string}/${Glob}/${string}`
+type AssetPath = Path
+type ContentPath = Path
 
 export const FieldTypes = z.enum([
 	'boolean',
@@ -66,9 +69,11 @@ export type Schema<T extends SchemaKey> = Record<T, Field>
 type CollectionSlug = z.infer<typeof COLLECTION_SLUG_REGEX>
 
 export type Collection = {
-	format?: CollectionFormat
 	label: string
-	path: ContentPath
+	paths: {
+		assets?: AssetPath | string
+		content: ContentPath | string
+	}
 	schema: Schema<SchemaKey>
 	slug: CollectionSlug
 }
@@ -77,6 +82,36 @@ export interface Collections {
 	[key: string]: Collection
 }
 
+// TODO: update cloud config properties
+const ASSETS_CLOUD_CONFIG = z.object({
+	type: z.enum(['r2', 's3']),
+	accessKey: z.string(),
+	secretKey: z.string(),
+	region: z.string(),
+	bucketName: z.string(),
+})
+const ASSETS_LOCAL_CONFIG = z.string()
+const ASSETS_CONFIG = z.union([ASSETS_CLOUD_CONFIG, ASSETS_LOCAL_CONFIG])
+
+const LOCAL_MODE = z.object({
+	format: CONTENT_FORMAT,
+	mode: z.literal('local'),
+	// TODO: add validation for assets and content paths
+	assets: ASSETS_CONFIG.optional(),
+	content: z.string().optional(),
+})
+
+const CLOUD_MODE = z.object({
+	mode: z.literal('cloud'),
+	assets: ASSETS_CONFIG,
+	// TODO: dsn can be a string or an object
+	content: z.object({ dsn: z.string() }),
+})
+
+const STORAGE = z.discriminatedUnion('mode', [LOCAL_MODE, CLOUD_MODE])
+type Storage = z.infer<typeof STORAGE>
+
 export type Config = {
 	collections: Collections
+	storage: Storage
 }
