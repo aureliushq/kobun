f13430795f7c95fdbd05b39aba80d1516506f421
Ilango <rajagopalilango@gmail.com>
2025-01-23 12:40:28 +0530

    Update collections and add logo images

--- demo/app/rescribe.config.ts
+++ demo/app/rescribe.config.ts
@@ -2,6 +2,43 @@ import { collection, config, fields } from '@rescribe/core'
 
 const rescribeConfig = config({
 	collections: {
+		articles: collection({
+			label: 'Articles',
+			slug: 'articles',
+			paths: {
+				assets: 'articles/**/*',
+				content: 'articles/**/*',
+			},
+			schema: {
+				title: fields.text({
+					label: 'Title',
+					description: 'Page Title',
+				}),
+				content: fields.document({
+					label: 'Content',
+				}),
+				published: fields.boolean({
+					component: 'checkbox',
+					label: 'Publish Page',
+					description:
+						'If the page should be published. Page will be saved as a draft by default.',
+				}),
+				publishedAt: fields.date({
+					label: 'Publish Date',
+					description: 'Set the publish date of the page',
+				}),
+				slug: fields.slug({
+					name: {
+						label: 'Post Title',
+						description: 'Title of the post',
+					},
+					slug: {
+						label: 'Slug',
+						description: 'Slug of the post',
+					},
+				}),
+			},
+		}),
 		docs: collection({
 			label: 'Docs',
 			slug: 'docs',
@@ -28,8 +65,14 @@ const rescribeConfig = config({
 					description: 'Set the publish date of the page',
 				}),
 				slug: fields.slug({
-					label: 'Page URL',
-					description: 'URL of the page',
+					name: {
+						label: 'Post Title',
+						description: 'Title of the post',
+					},
+					slug: {
+						label: 'Slug',
+						description: 'Slug of the post',
+					},
 				}),
 			},
 		}),
