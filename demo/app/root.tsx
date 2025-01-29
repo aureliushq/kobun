78a9d5883cf56e6a5dadf8e207bee90ae39ef9bc
Ilango <rajagopalilango@gmail.com>
2025-01-29 09:59:40 +0530

    Update demo

--- demo/app/root.tsx
+++ demo/app/root.tsx
@@ -14,12 +14,12 @@ export const links: Route.LinksFunction = () => [
 	{ rel: 'preconnect', href: 'https://fonts.googleapis.com' },
 	{
 		rel: 'preconnect',
-		href: 'https://fonts.gstatic.com',
+		href: 'https://fonts.bunny.net',
 		crossOrigin: 'anonymous',
 	},
 	{
 		rel: 'stylesheet',
-		href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
+		href: 'https://fonts.bunny.net/css?family=inter:400,500,600,700" rel="stylesheet',
 	},
 	{ rel: 'stylesheet', href: stylesheet },
 ]
@@ -36,7 +36,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 				<Meta />
 				<Links />
 			</head>
-			<body>
+			<body className='dark font-sans'>
 				{children}
 				<ScrollRestoration />
 				<Scripts />
--- demo/app/routes/home.tsx
+++ demo/app/routes/home.tsx
@@ -1,20 +1,76 @@
-import { Rescribe } from '@rescribe/core'
-import rescribeConfig from '~/rescribe.config'
-import '@rescribe/core/rescribe.css'
+import { Link } from 'react-router'
+import { Button } from '~/components/ui/button'
 
-export function meta() {
-	return [
-		{ title: 'New React Router App' },
-		{ name: 'description', content: 'Welcome to React Router!' },
-	]
+const Home = () => {
+	return (
+		<div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-20 gap-16'>
+			<main className='flex flex-col gap-8 row-start-2 px-4'>
+				<img
+					src='/logo-dark.png'
+					alt='Rescribe logo'
+					width={180}
+					height={38}
+				/>
+				<ol>
+					<li>
+						Get started by editing{' '}
+						<code className='px-1 py-2 rounded-[4px] font-semibold'>
+							rescribe.config.ts
+						</code>
+						.
+					</li>
+					<li>Save and see your changes instantly.</li>
+				</ol>
+
+				<div className='flex gap-4'>
+					<Link to='/rescribe' rel='noopener noreferrer'>
+						<Button size='sm'>Rescribe</Button>
+					</Link>
+					<Link
+						to='https://rescribe.site/docs'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						<Button size='sm' variant='secondary'>
+							Rescribe Documentation
+						</Button>
+					</Link>
+				</div>
+			</main>
+			<footer className='flex gap-6 row-start-3'>
+				<a
+					className='flex items-center gap-2'
+					href='https://remix.run/docs/en/main'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					<img
+						aria-hidden
+						src='/remix_logo_dark.png'
+						alt='Globe icon'
+						width={16}
+						height={16}
+					/>
+					Remix Documentation →
+				</a>
+				<a
+					className='flex items-center gap-2'
+					href='https://reactrouter.com/'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					<img
+						aria-hidden
+						src='/rr_logo_dark.png'
+						alt='Globe icon'
+						width={16}
+						height={16}
+					/>
+					React Router v7 Documentation →
+				</a>
+			</footer>
+		</div>
+	)
 }
 
-export const loader = async () => {
-	return { message: 'Hello from the server!' }
-}
-
-const CMS = () => {
-	return <Rescribe config={rescribeConfig} />
-}
-
-export default CMS
+export default Home
