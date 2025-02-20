2f78708828741a7ea4840801397edb989e8eac54
Ilango <rajagopalilango@gmail.com>
2025-02-20 21:00:10 +0530

    Set up server package - package.json and tsup config

---
+++ packages/server/tsup.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'tsup'
+import type { Options } from 'tsup'
+
+const config: Options = {
+	replaceNodeEnv: true,
+	splitting: true,
+	clean: false,
+	format: ['esm'],
+	skipNodeModulesBundle: true,
+	dts: true,
+	entry: ['src/index.ts'],
+	outDir: 'dist',
+	bundle: true,
+	minify: true,
+	name: '@rescribejs/server',
+	external: [],
+}
+
+export default defineConfig(config)
