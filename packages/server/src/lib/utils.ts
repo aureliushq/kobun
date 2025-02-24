cfdd54c51517d9da7cc8e0f2bd2960d4173b3c83
Ilango <rajagopalilango@gmail.com>
2025-02-24 14:33:11 +0530

    Add function to read a single collection item

--- packages/server/src/lib/utils.ts
+++ packages/server/src/lib/utils.ts
@@ -2,16 +2,19 @@ import { APP_BASE_PATH, type Collection } from '@rescribe/common'
 import fg from 'fast-glob'
 import matter from 'gray-matter'
 import fs from 'node:fs/promises'
+import type z from 'zod'
+
+type LocalCollectionContentPath = {
+	collection: Collection
+	format: 'md' | 'mdx'
+	slug?: string
+}
 
 export const getLocalCollectionContentPath = ({
 	collection,
 	format = 'md',
 	slug = '',
-}: {
-	collection: Collection
-	format: 'md' | 'mdx'
-	slug?: string
-}) => {
+}: LocalCollectionContentPath) => {
 	if (slug) {
 		// TODO: replace should check for glob pattern from config
 		return `${process.cwd()}${APP_BASE_PATH}/content/collections/${collection.paths.content
@@ -25,15 +28,40 @@ export const getLocalCollectionContentPath = ({
 export const readItemsInLocalCollection = async ({
 	collection,
 	format,
-}: { collection: Collection; format: 'md' | 'mdx' }) => {
+}: Omit<LocalCollectionContentPath, 'slug'>) => {
 	const path = getLocalCollectionContentPath({ collection, format })
 	const entries = await fg(path, { onlyFiles: true })
 	const data = await Promise.all(
 		entries.map(async (entry) => {
 			const file = await fs.readFile(entry, 'utf8')
-			const { data: frontmatter } = matter(file)
-			return frontmatter
+			const { data: metadata } = matter(file)
+			return metadata
 		}),
 	)
 	return data
+}
+
+type LocalCollectionItemPath = {
+	id: string
+	schema: z.ZodType
+} & Omit<LocalCollectionContentPath, 'slug'>
+
+export const readItemInLocalCollection = async ({
+	collection,
+	format = 'md',
+	id,
+	schema,
+}: LocalCollectionItemPath) => {
+	// TODO: use the index to get the file name instead of parsing every file
+	const items = await readItemsInLocalCollection({ collection, format })
+	const data = items.find((item) => item.id === id)
+	const metadata = schema.parse(data)
+	const path = getLocalCollectionContentPath({
+		collection,
+		format,
+		slug: metadata.slug,
+	})
+	const file = await fs.readFile(path, 'utf8')
+	const { content } = matter(file)
+	return { content, ...metadata }
 }
