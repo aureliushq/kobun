f1ddde9e9cd5298607666ed72ffe0d3b59f5f13d
Ilango <rajagopalilango@gmail.com>
2026-03-03 19:31:53 +0530

    add: login page

--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,6 @@
-import { type RouteConfig, route } from "@react-router/dev/routes";
+import { index, type RouteConfig, route } from "@react-router/dev/routes";
 
 export default [
+	index("routes/login.tsx"),
 	route("/component-example", "routes/example.tsx"),
 ] satisfies RouteConfig;
