3d22c4471a50d99ceefa83fe638034e5c7d66aa8
Ilango <rajagopalilango@gmail.com>
2026-03-06 00:39:36 +0530

    fix: tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, devices } from "@playwright/test"
 
 const baseURL = process.env.TEST_PAGE_URL
 	? process.env.TEST_PAGE_URL
-	: "http://localhost:3000"
+	: "http://localhost:5173"
 
 /**
  * See https://playwright.dev/docs/test-configuration.
