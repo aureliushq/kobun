0c6ae421ce8079707de0c62af81741212323bb09
Ilango <rajagopalilango@gmail.com>
2026-03-13 13:50:46 +0530

    update: move dashboard action and loader to layout component

--- app/core/components/layouts/dashboard.tsx
+++ app/core/components/layouts/dashboard.tsx
@@ -1,13 +1,61 @@
-import { Outlet } from "react-router"
+import { desc, eq } from "drizzle-orm"
+import { Outlet, redirect } from "react-router"
+import { getAuth } from "@/auth/auth.server"
+import { envContext } from "@/core/context"
+import { dbContext } from "@/db/context"
+import { project } from "@/db/schema/app-schema"
+import type { Project } from "@/db/types"
 import { ScrollArea } from "@/ui/components/base/scroll-area"
 import { SidebarProvider } from "@/ui/components/base/sidebar"
 import DashboardHeader from "@/ui/components/blocks/dashboard-header"
 import DashboardSidebar from "@/ui/components/blocks/dashboard-sidebar"
+import { PATHS } from "@/ui/lib/constants"
+import { DashboardActionIntents } from "@/ui/lib/types"
+import type { Route } from "./+types/dashboard"
 
-const DashboardLayout = () => {
+export async function loader({ context, params, request }: Route.LoaderArgs) {
+	const db = context.get(dbContext)
+	const env = context.get(envContext)
+
+	const auth = getAuth(env)
+	const session = await auth.api.getSession({ headers: request.headers })
+	if (!session?.user) throw redirect(PATHS.LOGIN)
+
+	const { owner, name } = params
+
+	const projects = await db.query.project.findMany({
+		where: eq(project.userId, session.user.id),
+		orderBy: desc(project.updatedAt),
+	})
+	const activeProject = projects.find(
+		(project) => project.repoOwnerLogin === owner && project.repoName === name,
+	)
+
+	return { activeProject, projects, user: session.user }
+}
+
+export async function action({ context, request }: Route.ActionArgs) {
+	const auth = getAuth(context.get(envContext))
+	const formData = await request.formData()
+	const intent = formData.get("intent")
+	if (intent === DashboardActionIntents.LOGOUT) {
+		const response = await auth.api.signOut({
+			asResponse: true,
+			headers: request.headers,
+		})
+		if (response.ok) {
+			return redirect(PATHS.LOGIN, { headers: response.headers })
+		}
+	}
+}
+
+const DashboardLayout = ({ loaderData }: Route.ComponentProps) => {
 	return (
 		<SidebarProvider>
-			<DashboardSidebar />
+			<DashboardSidebar
+				activeProject={loaderData.activeProject as Project}
+				projects={loaderData.projects}
+			/>
 			<main className="flex h-screen w-screen flex-col gap-4">
 				<DashboardHeader />
 				<ScrollArea className="z-10 h-full w-full p-8">
--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,34 +1,5 @@
-import { redirect } from "react-router"
-import { getAuth } from "@/auth/auth.server"
-import { envContext } from "@/core/context"
-import { PATHS } from "@/ui/lib/constants"
-import type { Route } from "./+types/dashboard"
+// export async function loader({ context, request }: Route.LoaderArgs) {}
 
-export async function loader({ context, request }: Route.LoaderArgs) {
-	const env = context.get(envContext)
-
-	const auth = getAuth(env)
-	const session = await auth.api.getSession({ headers: request.headers })
-	if (!session?.user) throw redirect(PATHS.LOGIN)
-
-	return { user: session.user }
-}
-
-export async function action({ context, request }: Route.ActionArgs) {
-	const auth = getAuth(context.get(envContext))
-	const formData = await request.formData()
-	const intent = formData.get("intent")
-	if (intent === "logout") {
-		const response = await auth.api.signOut({
-			asResponse: true,
-			headers: request.headers,
-		})
-		if (response.ok) {
-			return redirect(PATHS.LOGIN, { headers: response.headers })
-		}
-	}
-}
-
-export default function IndexRoute({ loaderData }: Route.ComponentProps) {
-	return <div>Welcome, {loaderData.user.email}</div>
+export default function IndexRoute() {
+	return <div>Welcome!</div>
 }
